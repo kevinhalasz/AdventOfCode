b94466ed0cf37c419027cbce2dd31dde1bf00067
Kevin Halasz <Dr.Kevin.Halasz@gmail.com>
2022-01-03 22:31:58 -0800

    The second half of the day 13 problem

--- 13Day/Origami.py
+++ 13Day/Origami.py
@@ -47,18 +47,37 @@ def mark_dots(dots):
     return grid
 
 
-def x_fold(grid,line):
+def x_fold(grid,intercept):
     # We make a copy of the right-hand side of the grid
-    new_grid = grid[:,line+1:].copy()
+    new_grid = grid[:,:intercept].copy()
     
     # Then make a copy of the left-hand side before folding
-    old_grid = grid[:,:line].copy()
+    old_grid = grid[:,intercept+1:].copy()
     
     # Find where the dots are on the left-hand side
     y_true, x_true = np.where(old_grid==True)
     
-    # Then project the x-coordinates across x=line
-    x_true = line-1-x_true
+    # Then project the x-coordinates across x=intercept
+    x_true = intercept-1-x_true
+    
+    # Mark the dots which appear after folding
+    for point in zip(y_true,x_true):
+        new_grid[point] = True
+    
+    return new_grid
+
+
+
+def y_fold(grid,intercept):
+    new_grid = grid[:intercept,:].copy()
+    
+    # Then make a copy of the left-hand side before folding
+    old_grid = grid[intercept+1:,:].copy()
+    
+    # Find where the dots are on the left-hand side
+    y_true, x_true = np.where(old_grid==True)
+    
+    y_true = intercept-1-y_true
     
     # Mark the dots which appear after folding
     for point in zip(y_true,x_true):
@@ -69,9 +88,20 @@ def x_fold(grid,line):
 def score(grid):
     return np.sum(grid)
 
+def folding(grid,folds):
+    for fold in folds:
+        if fold[0] == 'x':
+            grid = x_fold(grid,int(fold[1]))
+        if fold[0] == 'y':
+            grid = y_fold(grid,int(fold[1]))
+    return grid
+
 if __name__=='__main__':
     dots, folds = unpack_data()
     grid = mark_dots(dots)
     # This is kind of cheating, because I know the first fold is an x-fold
-    grid = x_fold(grid,int(folds[0][1]))
-    print(score(grid))
+    grid = folding(grid,folds)
+    grid = grid.astype(int)
+    for i in range(8):
+        print(str(grid[:,5*i:5*(i+1)])+'\n')
+
